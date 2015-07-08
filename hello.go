5d70606d7d53f0ea404da164d981500bf299269f
Karthick <karthick@vakilsearch.com>
2015-07-08 15:41:53 +0530

    current date added

--- hello.go
+++ hello.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+
+import (
+    "fmt";
+    "strings";
+	"time";
+)
 
 func main() {
-    fmt.Printf("hello, world\n")
+	fmt.Printf("hello, world\n")
+    s := []string{"this", "is", "a", "joined", "string\n"};
+    fmt.Printf(strings.Join(s, " "));
+	fmt.Printf("The time is", time.Now())
 }
